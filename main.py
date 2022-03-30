03412a4d502dadd75a5c071357119d958e19ceb6
conorlyons99 <clyons.lyons@gmail.com>
2022-03-30 17:05:19 +0100

    Fixed crash issue on startup and added minor changes

--- main.py
+++ main.py
@@ -8,16 +8,17 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 app = create_app()
 
-df2 = pd.read_csv('./model/tmdb.csv')
+df2 = pd.read_csv("./model/tmdb.csv")
 
-count = CountVectorizer(stop_words='english')
-count_matrix = count.fit_transform(df2['soup'])
+count = CountVectorizer(stop_words="english")
+count_matrix = count.fit_transform(df2["soup"].values.astype("U"))
 
 cosine_sim2 = cosine_similarity(count_matrix, count_matrix)
 
 df2 = df2.reset_index()
-indices = pd.Series(df2.index, index=df2['title'])
-all_titles = [df2['title'][i] for i in range(len(df2['title']))]
+indices = pd.Series(df2.index, index=df2["title"])
+all_titles = [df2["title"][i] for i in range(len(df2["title"]))]
+
 
 def get_recommendations(title):
     cosine_sim = cosine_similarity(count_matrix, count_matrix)
@@ -26,46 +27,62 @@ def get_recommendations(title):
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     sim_scores = sim_scores[1:11]
     movie_indices = [i[0] for i in sim_scores]
-    tit = df2['title'].iloc[movie_indices]
-    dat = df2['release_date'].iloc[movie_indices]
-    gen = df2['genres'].iloc[movie_indices]
-    tag = df2['tagline'].iloc[movie_indices]
-    hom = df2['homepage'].iloc[movie_indices]
-    return_df = pd.DataFrame(columns=['Title','Year','Genres', 'Tagline', 'Homepage'])
-    return_df['Title'] = tit
-    return_df['Year'] = dat
-    return_df['Genres'] = gen
-    return_df['Tagline'] = tag
-    return_df['Homepage'] = hom
+    tit = df2["title"].iloc[movie_indices]
+    dat = df2["release_date"].iloc[movie_indices]
+    gen = df2["genres"].iloc[movie_indices]
+    ove = df2["overview"].iloc[movie_indices]
+    hom = df2["homepage"].iloc[movie_indices]
+    vot = df2["vote_average"].iloc[movie_indices]
+    return_df = pd.DataFrame(
+        columns=["Title", "Year", "Genres", "Overview", "Homepage", "Voter Average",]
+    )
+    return_df["Title"] = tit
+    return_df["Year"] = dat
+    return_df["Genres"] = gen
+    return_df["Overview"] = ove
+    return_df["Homepage"] = hom
+    return_df["Voter Average"] = vot
     return return_df
 
-@app.route('/', methods=['GET', 'POST'])
 
+@app.route("/", methods=["GET", "POST"])
 def main():
-    if flask.request.method == 'GET':
-        return(flask.render_template('index.html'))
-            
-    if flask.request.method == 'POST':
-        m_name = flask.request.form['movie_name']
+    if flask.request.method == "GET":
+        return flask.render_template("index.html")
+
+    if flask.request.method == "POST":
+        m_name = flask.request.form["movie_name"]
         m_name = m_name.title()
         if m_name not in all_titles:
-            return(flask.render_template('negative.html',name=m_name))
+            return flask.render_template("negative.html", name=m_name)
         else:
             result_final = get_recommendations(m_name)
             names = []
             dates = []
             genres = []
-            tagline = []
+            overview = []
             homepage = []
+            voterAvg = []
             for i in range(len(result_final)):
                 names.append(result_final.iloc[i][0])
                 dates.append(result_final.iloc[i][1])
                 genres.append(result_final.iloc[i][2])
-                tagline.append(result_final.iloc[i][3])
+                overview.append(result_final.iloc[i][3])
                 homepage.append(result_final.iloc[i][4])
+                voterAvg.append(result_final.iloc[i][5])
 
-            return flask.render_template('results.html',movie_names=names,movie_date=dates,movie_genres=genres,movie_tagline=tagline,movie_homepage=homepage,search_name=m_name)
+            return flask.render_template(
+                "results.html",
+                movie_names=names,
+                movie_date=dates,
+                movie_genres=genres,
+                movie_overview=overview,
+                movie_homepage=homepage,
+                movie_voterAvg=voterAvg,
+                search_name=m_name,
+            )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
-    
+
--- website/views.py
+++ website/views.py
@@ -1,24 +1,28 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 
-views = Blueprint('views',__name__)
+views = Blueprint("views", __name__)
 
-@views.route('/')
+
+@views.route("/")
 @login_required
 def home():
     return render_template("home.html", user=current_user)
 
-@views.route('/recommender')
+
+@views.route("/recommender")
 @login_required
 def recommender():
     return render_template("recommender.html", user=current_user)
 
-@views.route('/results')
+
+@views.route("/results")
 @login_required
 def results():
     return render_template("results.html", user=current_user)
 
-@views.route('/negative')
+
+@views.route("/negative")
 @login_required
 def negative():
     return render_template("negative.html", user=current_user)
